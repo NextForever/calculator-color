82a50638cbc1165544345cd6d827712d3f0e56b8
GezMin <s.shvetsov.v@gmail.com>
2023-02-01 22:04:04 +0600

    added sass redux router-dom

---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.scss';
+
+const Main = () => {
+    return (
+        <>
+            <h1 className='h1'>Calculator painting</h1>
+        </>
+    );
+};
+
+export default Main;
