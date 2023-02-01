29aea2d3f82cdbf9fe098444beb937d3d0c641a2
GezMin <s.shvetsov.v@gmail.com>
2023-02-01 23:45:14 +0600

    material UI

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import { useState } from 'react';
+import { Container, Grid, Radio } from '@mui/material';
+
 import './main.scss';
 
+let carTypes = [
+    { type: 'Sedan', img: 'sedan.jpg' },
+    { type: 'Hatchback', img: 'hatchback.jpg' },
+    { type: 'SUV', img: 'suv.jpg' },
+    { type: 'Coupe', img: 'coupe.jpg' },
+    { type: 'Convertible', img: 'convertible.jpg' },
+];
+
 const Main = () => {
+    const [selectedType, setSelectedType] = useState(null);
+
     return (
-        <>
-            <h1 className='h1'>Calculator painting</h1>
-        </>
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={3}>
+                    {carTypes.map(carType => (
+                        <div key={carType.type}>
+                            <Radio
+                                type='radio'
+                                id={carType.type}
+                                value={carType.type}
+                                checked={selectedType === carType.type}
+                                onChange={e => setSelectedType(e.target.value)}
+                            />
+                            <label htmlFor={carType.type}>{carType.type}</label>
+                        </div>
+                    ))}
+                </Grid>
+                <Grid item xs={12} md={9}>
+                    <div>
+                        {selectedType && <img src={selectedType.img} alt={selectedType.type} />}
+                    </div>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
