21b90c0bcfea9e16587df0a465eb89daaa72edc4
GezMin <s.shvetsov.v@gmail.com>
2023-02-01 21:40:22 +0600

    update project

--- src/_codux/boards/main-slider/main-slider.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { MainSlider } from '../../../components/main-slider/main-slider';
-
-export default createBoard({
-    name: 'MainSlider',
-    Board: () => <MainSlider />
-});
