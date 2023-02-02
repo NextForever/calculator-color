149087cc14d84cf34f9f31b6ec9e0ea7090fe526
GezMin <s.shvetsov.v@gmail.com>
2023-02-02 21:00:57 +0600

    img testing

--- src/components/navbar/navBar.js
+++ src/components/navbar/navBar.js
@@ -2,8 +2,15 @@ import './navBar.scss';
 
 const NavBar = () => {
     return (
-        <nav className='navigate'>
-            <h1>Painting Calculator</h1>
+        <nav className='nav'>
+            <div className='wrapper'>
+                <div className='nav-item'>
+                    <h1>Painting Calculator</h1>
+                </div>
+                <div className='nav-item'>Сумма</div>
+                <div className='nav-item'>--</div>
+                <div className='nav-item'>darkMode</div>
+            </div>
         </nav>
     );
 };
