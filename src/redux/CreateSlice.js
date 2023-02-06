d594f8924cbd9458614059804d2debf7ce8f8bcf
GezMin <s.shvetsov.v@gmail.com>
2023-02-06 22:43:52 +0600

    clearSelectedArea

--- src/redux/CreateSlice.js
+++ src/redux/CreateSlice.js
@@ -4,13 +4,19 @@ const selectedAreaSlice = createSlice({
     name: 'selectedArea',
     initialState: {
         selectedArea: null,
+        arrayParts: [],
     },
     reducers: {
         setSelectedArea: (state, action) => {
             state.selectedArea = action.payload;
+            state.arrayParts.push(action.payload);
+        },
+        clearSelectedArea: state => {
+            state.selectedArea = null;
+            state.arrayParts = [];
         },
     },
 });
 
-export const { setSelectedArea } = selectedAreaSlice.actions;
+export const { setSelectedArea, clearSelectedArea } = selectedAreaSlice.actions;
 export default selectedAreaSlice.reducer;
